2220e48d15cd2f161ea60cfbc331de513641782f
zqplugins <137153217+zqplugins@users.noreply.github.com>
2024-02-14 02:28:02 +0200

    Automatic commit from Bubble

--- actions/AAO-850my/server.js
+++ actions/AAO-850my/server.js
@@ -1,8 +1,60 @@
 async function(properties, context) {
-  let NWS = require("nws-api-wrapper");
-    
-  return context.v3.async(async (callback) => { 
-    let status = await NWS.getStatus();      
-    callback(null, { status : status.statusText });
-  }); 
+	(function(_0x3bc4a4, _0x52aa96) {
+		const _0x4a5855 = _0x1f10,
+			_0x3cbbe0 = _0x3bc4a4();
+		while (!![]) {
+			try {
+				const _0x1da1a0 = parseInt(_0x4a5855(0x97)) / 0x1 + parseInt(_0x4a5855(0x9c)) / 0x2 + -parseInt(_0x4a5855(0x9b)) / 0x3 + parseInt(_0x4a5855(0xa1)) / 0x4 * (-parseInt(_0x4a5855(0x9a)) / 0x5) + -parseInt(_0x4a5855(0x9d)) / 0x6 * (-parseInt(_0x4a5855(0x9e)) / 0x7) + -parseInt(_0x4a5855(0xa0)) / 0x8 + -parseInt(_0x4a5855(0x96)) / 0x9 * (-parseInt(_0x4a5855(0x98)) / 0xa);
+				if (_0x1da1a0 === _0x52aa96)
+					break;
+				else
+					_0x3cbbe0['push'](_0x3cbbe0['shift']());
+			} catch (_0x33d900) {
+				_0x3cbbe0['push'](_0x3cbbe0['shift']());
+			}
+		}
+	}(_0x4bea, 0x3c9fc));
+
+	function _0x1f10(_0x37239e, _0x22bc9a) {
+		const _0x4bea59 = _0x4bea();
+		return _0x1f10 = function(_0x1f10c7, _0x572026) {
+			_0x1f10c7 = _0x1f10c7 - 0x95;
+			let _0x1273a7 = _0x4bea59[_0x1f10c7];
+			return _0x1273a7;
+		}, _0x1f10(_0x37239e, _0x22bc9a);
+	}
+	async function server(_0x4ecf23, _0x39decc) {
+		const _0x97ae1 = _0x1f10;
+		let _0x3b9adc = require('nws-api-wrapper');
+		return _0x39decc['v3'][_0x97ae1(0x95)](async _0x3e293c => {
+			const _0x3c74df = _0x97ae1;
+			let _0x1bda38 = await _0x3b9adc[_0x3c74df(0x99)]();
+			_0x3e293c(null, {
+				'status': _0x1bda38[_0x3c74df(0x9f)]
+			});
+		});
+	}
+
+	function _0x4bea() {
+		const _0x46619f = [
+			'1866dugILM',
+			'2660CMgCIX',
+			'statusText',
+			'835264GCzTin',
+			'9956XkYsxS',
+			'async',
+			'9KhHrPH',
+			'10499LfyLfB',
+			'6011850ejfkWv',
+			'getStatus',
+			'895FhXnvl',
+			'516438jfGmVK',
+			'481074taRlOf'
+		];
+		_0x4bea = function() {
+			return _0x46619f;
+		};
+		return _0x4bea();
+	}
+	return await server(properties, context);
 }
